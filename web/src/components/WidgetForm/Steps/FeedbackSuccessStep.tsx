9b3dd6a1c3b1f6fc4577a9521236e825928ebec6
Gabriel Herter <gabriel.herter@dock.tech>
2022-05-05 08:41:40 -0300

    aula 2 1h01

---
+++ web/src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -0,0 +1,2 @@
+export function FeedbackSuccessStep(){
+    return()}
---
+++ web/src/components/WidgetForm/index.tsx
@@ -0,0 +1,53 @@
+import { CloseButton } from "../CloseButton";
+import bugImageUrl from "../../assets/bug.svg"
+import ideaImageUrl from "../../assets/idea.svg"
+import thoughtImageUrl from "../../assets/thought.svg"
+import {FeedbackTypeStep} from "./Steps/FeedbackTypeStep";
+import {useState} from "react";
+import {FeedbackContentStep} from "./Steps/FeedbackContentStep";
+
+
+export const feedbackTypes = {
+    BUG:{
+    title:"Problem",
+    image:{
+        source:bugImageUrl,
+        alt:"bug image"
+    }
+    },
+
+    IDEA:{
+        title:"Idea",
+        image:{
+            source:ideaImageUrl,
+            alt:"lamp image"
+        }
+    },
+
+    OTHER:{
+        title:"Other",
+        image:{
+            source:thoughtImageUrl,
+            alt:"thought ballon image"
+        }
+    }
+}
+
+export type FeedbackType =  keyof typeof feedbackTypes
+
+export function Index(){
+
+    const [feedbackType,setFeedbackType] = useState<FeedbackType | null>(null)
+
+return(
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+
+
+        {!feedbackType?<FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>:<FeedbackContentStep feedbackType={feedbackType}/>}
+
+        <footer className="text-xs text-neutral-400">
+            Made with ❤️ by <a className="underline underline-offset-1" href="https://www.linkedin.com/in/gabrielguinterherter/">Gabriel Guinter Herter</a>
+        </footer>
+    </div>
+)
+}
