9a928a31fa8e16476e00c7a907bab263bcb39303
Gabriel Herter <gabriel.herter@dock.tech>
2022-05-06 08:44:20 -0300

    aula 2 1h43

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,28 +1,69 @@
-import {CloseButton} from "../../CloseButton";
-import {FeedbackType, feedbackTypes} from "../index";
-import {ArrowLeft} from "phosphor-react";
+import { CloseButton } from "../../CloseButton";
+import { FeedbackType, feedbackTypes } from "../index";
+import { ArrowLeft, Camera } from "phosphor-react";
+import { ScreenshotButton } from "../Screenshotbutton";
+import { FormEvent, useState } from "react";
 
 interface FeedbackContentStepProps {
-    feedbackType: FeedbackType
-    onFeedbackRestartRequested:()=>void
+  feedbackType: FeedbackType;
+  onFeedbackRestartRequested: () => void;
 }
 
-export function FeedbackContentStep({feedbackType,onFeedbackRestartRequested}:FeedbackContentStepProps){
-    const feedbackTypeInfo = feedbackTypes[feedbackType];
+export function FeedbackContentStep({
+  feedbackType,
+  onFeedbackRestartRequested,
+}: FeedbackContentStepProps) {
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
+  const [screenshot, setScreenshot] = useState<string | null>(null);
+  const [comment, setcomment] = useState("");
 
-    return(
-        <>
-            <header>
-                <button type="button" className = "top-5 left-5 absolute text-zinc-400 hover:text-zinc-100" onClick={onFeedbackRestartRequested}>
-                    <ArrowLeft weight="bold" className="h-4 w-4"/>
-                </button>
-                <span className = "text-xl leading-6 flex items-center gap-2"> <img className = "w-6 h-6" src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt}/>  {feedbackTypeInfo.title}</span>
-                <CloseButton/>
-            </header>
+  function handleSubmitFeedback(event: FormEvent) {
+    event.preventDefault();
+    console.log(screenshot);
+    console.log(comment);
+  }
 
-            <form className = "my-4 w-full ">
-                <textarea className = "min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-400 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none focusoutline-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin" placeholder="Tell me what's going on..."/>
-            </form>
-        </>
-    )
+  return (
+    <>
+      <header>
+        <button
+          type="button"
+          className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+          onClick={onFeedbackRestartRequested}
+        >
+          <ArrowLeft weight="bold" className="h-4 w-4" />
+        </button>
+        <span className="text-xl leading-6 flex items-center gap-2">
+          <img
+            className="w-6 h-6"
+            src={feedbackTypeInfo.image.source}
+            alt={feedbackTypeInfo.image.alt}
+          />
+          {feedbackTypeInfo.title}
+        </span>
+        <CloseButton />
+      </header>
+
+      <form className="my-4 w-full" onSubmit={handleSubmitFeedback}>
+        <textarea
+          className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-400 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none focusoutline-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+          placeholder="Tell me what's going on..."
+          onChange={(event) => setcomment(event.target.value)}
+        />
+
+        <footer className="flex gap-2 mt-2">
+          <ScreenshotButton
+            onScreenshotTook={setScreenshot}
+            screenshot={screenshot}
+          />
+          <button
+            type="submit"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+          >
+            Send
+          </button>
+        </footer>
+      </form>
+    </>
+  );
 }
