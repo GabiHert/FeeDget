b2d5d45748d77d19473351db1691b0f3c4ae0048
Gabriel Herter <gabriel.herter@dock.tech>
2022-05-08 01:02:10 -0300

    finalizada aula 3

--- web/server/src/server.ts
+++ web/server/src/server.ts
@@ -1,20 +1,11 @@
 import express from "express"
-import {prisma} from "./prisma";
-import {nodemailer} from "nodemailer"
+import cors from "cors"
+import {routes} from "./routes";
 
 const app = express()
-
+app.use(cors())
 app.use(express.json())
-
-app.post("/feedbacks", async (req, res) => {
-    const {type, comment, screenshot} = req.body
-
-    const feedback = await prisma.feedback.create({
-        data: {type, comment, screenshot}
-    })
-
-    return res.status(201).json({data: feedback})
-})
+app.use(routes);
 
 app.listen(3333, () => {
     console.log("Http server listening http://localhost:3333")
